979ac52bbcc14a7bdf691c5e954026e67dc36ccf
nermoo <53093663+nermoo@users.noreply.github.com>
2021-02-09 23:18:51 +0530

    minor changes

--- src/components/bottomNav.js
+++ src/components/bottomNav.js
@@ -5,6 +5,8 @@ import { IconButton } from '@material-ui/core';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import  {makeStyles}  from '@material-ui/core/styles';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Typography,Box } from '@material-ui/core';
 
 
 
@@ -14,6 +16,24 @@ const useStyles=makeStyles({
         fontSize:'large',
         color:'primary',
     },
+    section:{
+        display:'flex',
+        justifyContent:'center',
+        backgroundColor:'#757575',
+        opacity:'0.8',
+        color:'solidBlack'
+    },
+    address:{
+       display:'block',
+       textAlign:'center',
+       color:'black',
+       opacity:'1',
+    },
+    bottom:{
+        height:'100%',
+        position:'relative',
+        overflow:'auto',
+    }
 });
 
 const BottomNav=()=>{
@@ -21,17 +41,22 @@ const BottomNav=()=>{
     const classes=useStyles();
 
     return(
-        <BottomNavigation>
+        <BottomNavigation className={classes.bottom}>
             <Grid container direction='column '>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className={classes.section}>
 
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className={classes.section}>
+                    <Typography className={classes.address}>
+                        <Box fontSize="h6.fontSize">Address</Box>
+                        <Box>Kurunegala</Box>
+                    </Typography>
 
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className={classes.section}>
                     <FacebookIcon className={classes.social}/>
                     <TwitterIcon className={classes.social}/>
+                    <LinkedInIcon className={classes.social}/>
 
                 </Grid>
             </Grid>
